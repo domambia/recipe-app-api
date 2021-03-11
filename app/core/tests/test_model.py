586ec950bc98935ed6a31e95de4c64437896e4b1
macs-code <omambiadauglous@gmail.com>
2021-03-11 17:41:02 +0300

    Fix  travis and flake 8

---
+++ app/core/tests/test_model.py
@@ -0,0 +1,10 @@
+from django.test import TestCase 
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+
+    def test_create_user_successful(self):
+        """Test creating user using email succesful"""
+        pass
